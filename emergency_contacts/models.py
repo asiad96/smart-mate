e7a73dd1a74ae3a6ef35bafb381461e5c2419a61
Anastasia Dwarica <adwarica96@gmail.com>
2024-09-21 17:55:09 -0500

    Created contact model and added django-phonenumber-field for phone number validation

--- emergency_contacts/models.py
+++ emergency_contacts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+#  to validates format, pretty print and convert phone numbers
+
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # phone_number not optional
+    phone_number = PhoneNumberField(blank=False)
--- smartmate/settings.py
+++ smartmate/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "emergency_contacts.apps.EmergencyContactsConfig",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
