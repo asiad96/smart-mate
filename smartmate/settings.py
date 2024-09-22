9f7b4466ac6a41ec4e2b77f859e76f4063bffcff
Anastasia Dwarica <adwarica96@gmail.com>
2024-09-22 14:27:21 -0500

    Add STATIC_ROOT for collectstatic

--- smartmate/settings.py
+++ smartmate/settings.py
@@ -127,11 +127,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
